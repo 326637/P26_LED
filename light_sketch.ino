7b8e6718464ccd1db9cbebd22fca3a3ff9023f3c
P26 <36890081+326637@users.noreply.github.com>
2018-03-01 09:08:07 -0500

    Update light_sketch.ino

--- light_sketch.ino
+++ light_sketch.ino
@@ -1,17 +1,20 @@
 int potPin= A0;  //Declare potPin to be analog pin A0
+int menuswPin= 6; //switch to turn on menu edit mode pin 6
 int LEDPin= 9;  // Declare LEDPin to be arduino pin 9
 int readValue;  // Use this variable to read Potentiometer
 int BLED; // LED brightness
+int GLED; //Goal LED setting
+int MLED; //MAX LED setting
 int TIME= 0; // time right now
- 
+
 void setup() {
   pinMode(potPin, INPUT);  //set potPin to be an input
   pinMode(LEDPin, OUTPUT); //set LEDPin to be an OUTPUT
+  pinMode(menuswPin, INPUT); //set menuswitch on pin to input
   Serial.begin(9600);      // turn on Serial Port
 }
  
-void loop() {
-  
+void loop() { 
  readValue = analogRead(potPin);  //Read the voltage on the Potentiometer
  BLED = (255./1023.) * readValue; //Calculate Write Value for LED
  analogWrite(LEDPin, BLED);      //Write to the LED
@@ -20,4 +23,5 @@ void loop() {
  TIME++;
  Serial.print(TIME);
 }
+
 
